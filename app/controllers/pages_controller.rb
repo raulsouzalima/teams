51f3437a61a9d0a6fe6c28030bc3f73b268cd400
Raul Souza Lima <raulsouzalima@gmail.com>
2012-09-15 22:41:02 -0300

    List teams on index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def index
+    doc = Nokogiri::HTML(open('http://globoesporte.globo.com/').read)
+    @teams = doc.search('div#1 .ativo a')
   end
 end
