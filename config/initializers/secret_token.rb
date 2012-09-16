4d486e87ed4cafbc321723acafdfd02b9b321d05
Raul Souza Lima <raulsouzalima@gmail.com>
2012-09-15 21:23:59 -0300

    Created project, added haml and added pages controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Team::Application.config.secret_token = 'cc8dfd92b7a5aa5a0205226eddaf1482c7069c8adfb3e70243626c873b1ea3200e11457510bde096b3ade8d38267888e40d2fcabc1a746cdb91a6fa768520d92'
